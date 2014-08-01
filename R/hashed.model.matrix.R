9776bbeff3be47c18f53779f6567ae96ae83a3d0
Wush Wu <wush978@gmail.com>
2014-08-01 08:10:12 +0800

    implement hashed.model.matrix with no constrasts

--- R/hashed.model.matrix.R
+++ R/hashed.model.matrix.R
@@ -2,9 +2,20 @@
 #'@importFrom Matrix sparse.model.matrix
 #'@export
 hashed.model.matrix <- function(object, data = environment(object),
-  constrasts.arg = NULL, xlev = NULL, transpose = FALSE, 
+  xlev = NULL, transpose = FALSE, 
   drop.unused.levels = FALSE, row.names = TRUE, verbose = FALSE, 
   hash_size = 2^24, ...) {
-  
+  .data.class <- sapply(data, class, simplify = FALSE, USE.NAMES = TRUE)
+  .data.categoric.i <- sapply(.data.class, simplify = TRUE, function(s) {
+    ("character" %in% s) | ("factor" %in% s)
+  })
+  .data.categoric <- names(data)[.data.categoric.i]
+  contrasts.arg <- sapply(.data.categoric, simplify = FALSE, USE.NAMES = TRUE,
+    function(name) {
+      contrasts(data[[name]], contrasts = FALSE)
+  })
+  m <- sparse.model.matrix(object, data, contrasts.arg, xlev, transpose,
+                           drop.unused.levels, row.names, verbose, ...)
+  hash_internal(m)
 }
   

60b3e1d4dbfa0a325e49ace1e7158f2e7d837bab
Wush Wu <wush978@gmail.com>
2014-10-10 15:26:29 +0800

    add Xv and vX test for checking conversion between CSRMatrix and dgCMatrix

--- tests/test-conver-to-dgCMatrix.R
+++ tests/test-conver-to-dgCMatrix.R
@@ -12,6 +12,19 @@ m5 <- as(m4, "dgCMatrix")
 invisible(capture.output(print(m5)))
 stopifnot(isTRUE(all.equal(dim(m4), dim(m5))))
 stopifnot(isTRUE(all.equal(sum(m4@x), sum(m5@x))))
-r1 <- m4 %*% (v <- rnorm(dim(m4)[2]))
-r2 <- as.vector(m5 %*% v)
-stopifnot(isTRUE(all.equal(r1, r2)))
+for(i in seq_len(dim(m4)[2])) {
+  e1 <- numeric(dim(m4)[2])
+  e1[i] <- 1.0
+  r4 <- m4 %*% e1
+  r5 <- as.vector(m5 %*% e1)
+  stopifnot(isTRUE(all.equal(r4, r5)))
+}
+
+for(i in seq_len(dim(m4)[1])) {
+  e1 <- numeric(dim(m4)[1])
+  e1[i] <- 1.0
+  r4 <- e1 %*% m4
+  r5 <- as.vector(e1 %*% m5)
+  stopifnot(isTRUE(all.equal(r4, r5)))
+}
+

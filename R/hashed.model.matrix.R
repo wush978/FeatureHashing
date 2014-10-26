6b5a843cb6a7aa9ef90104ae9dd86fbee9645150
Wush Wu <wush978@gmail.com>
2014-10-27 00:52:03 +0800

    remove original hashed.model.matrix

--- R/hashed.model.matrix.R
+++ R/hashed.model.matrix.R
@@ -1,24 +1,20 @@
-#'@title Feature Hashing of Model Matrix
-#'@param hash_size integer value. The size of hashed variables.
-#'@inheritParams Matrix::sparse.model.matrix
-#'@seealso \code{\link{sparse.model.matrix}}
-#'@importFrom Matrix sparse.model.matrix
+#'@title Create a Model Matrix with Feature Hashing
+#'@param object formula. A symbolic description of the model.
+#'The details of model specification are given under 'Details'.
+#'@param data data.frame. The original data.
+#'@param hash_size positive integer. The hash size of feature hashing.
+#'@param keep.hashing.mapping logical value. 
+#'The indicator of whether store the hash mapping.
 #'@export
-hashed.model.matrix <- function(object, data = environment(object),
-  contrasts.arg = NULL, xlev = NULL, transpose = TRUE, 
-  drop.unused.levels = FALSE, row.names = TRUE, verbose = FALSE, 
-  hash_size = 2^24, ...) {
-  m <- sparse.model.matrix(object, data, contrasts.arg, xlev, transpose,
-                           drop.unused.levels, row.names, verbose)
-  if (verbose) cat(sprintf("original feature size is %d\n", nrow(m)))
-  if (is.null(hash_size)) return(m)
-  mapping <- hash_without_intercept(rownames(m))
-  if (verbose) cat(sprintf("estimated collision rate is %0.8f\n", sum(duplicated(mapping)) / length(mapping)))
-  rehash_inplace(m, mapping, hash_size)
-  class(m) <- .CSRMatrix
-  m@Dim[1] <- as.integer(hash_size)
-  m@Dimnames[[1]] <- character(0)
-  m
+#'@importFrom methods new
+#'@importFrom methods checkAtAssignment
+hashed.model.matrix <- function(object, data = environment(object), hash_size = 2^24, keep.hashing_mapping = FALSE) {
+  tf <- terms.formula(object, data = data, specials = "tag")
+  retval <- new(.CSRMatrix)
+  .hashed.model.matrix(tf, data, hash_size, retval, keep.hashing_mapping)
+  class(retval) <- .CSRMatrix
+  retval@Dimnames[[2]] <- paste(seq_len(retval@Dim[2]))
+  retval
 }
 
 parse_tag <- function(text) {
@@ -42,16 +38,4 @@ parse_tag <- function(text) {
     }
     list(reference_name = reference_name, split = split, type = type)
   }, finally = {options(keep.source = origin.keep.source)})
-}
-
-#'@importFrom methods new
-#'@importFrom methods checkAtAssignment
-#'@export
-hashed.model.matrix2 <- function(object, data = environment(object), hash_size = 2^24, keep.hashing_mapping = FALSE) {
-  tf <- terms.formula(object, data = data, specials = "tag")
-  retval <- new(.CSRMatrix)
-  .hashed.model.matrix(tf, data, hash_size, retval, keep.hashing_mapping)
-  class(retval) <- .CSRMatrix
-  retval@Dimnames[[2]] <- paste(seq_len(retval@Dim[2]))
-  retval
 }

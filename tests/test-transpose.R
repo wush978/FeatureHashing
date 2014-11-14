627a387e096a3e40d541cec73e59b07110be4b3a
Wush Wu <wush978@gmail.com>
2014-11-15 01:55:51 +0800

    implement transpose and test scripts

---
+++ tests/test-transpose.R
@@ -0,0 +1,7 @@
+library(FeatureHashing)
+m1 <- as(hashed.model.matrix(~ ., CO2, 2^5), "dgCMatrix")
+m2 <- as(hashed.model.matrix(~ ., CO2, 2^5, F), "dgCMatrix")
+
+m1.1 <- as(m1, "matrix")
+m2.1 <- as(m2, "matrix")
+stopifnot(all(t(m1.1) == m2.1))

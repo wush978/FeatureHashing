079d4913eacfaf5a0afc8bb6683a40abcb15e2c7
Wush Wu <wush978@gmail.com>
2014-11-22 10:04:24 +0800

    documentation rename CSRMatrix to CSCMatrix

--- R/hashed.model.matrix.R
+++ R/hashed.model.matrix.R
@@ -1,10 +1,36 @@
 #'@title Create a Model Matrix with Feature Hashing
-#'@param object formula. A symbolic description of the model.
-#'The details of model specification are given under 'Details'.
+#'@param object formula. A model formula.
 #'@param data data.frame. The original data.
 #'@param hash_size positive integer. The hash size of feature hashing.
-#'@param keep.hashing.mapping logical value. 
+#'@param transpose logical value. Indicating if the transpose should be returned.
+#'@param keep.hashing_mapping logical value. 
 #'The indicator of whether store the hash mapping.
+#'
+#'@details
+#'The \code{hashed.model.matrix} hashes the feature automatically during
+#'the construction of the model matrix. It uses the 32-bit variant of MurmurHash3 
+#'\url{https://code.google.com/p/smhasher/wiki/MurmurHash3}. Weinberger 
+#'et. al. (2009) used two seperate hashing function \eqn{h} and \eqn{\xi}
+#'to determine the index and the sign of a feature, respectively. Different
+#'seed is used to implement the hashing function \eqn{h} and \eqn{\xi} with
+#'MurmurHash3.
+#'
+#'@references Kilian Q. Weinberger, Anirban Dasgupta, John Langford, 
+#'Alexander J. Smola, and Josh Attenberg. ICML, volume 382 of ACM 
+#'International Conference Proceeding Series, page 140. ACM, (2009)
+#'
+#'@examples
+#'# Construct the model matrix. The transposed matrix is returned by default.
+#'m <- hashed.model.matrix(~ ., CO2, 8, keep.hashing_mapping = TRUE)
+#'# Print the matrix via dgCMatrix
+#'as(m, "dgCMatrix")
+#'# Check the result of hashing
+#'ls(attr(m, "mapping"))
+#'# The row of "conc" is collided with the intercept if the hash size is 8
+#'attr(m, "mapping")[["conc"]] %% 8
+#'# The result is CSCMatrix which supports simple subsetting and matrix-vector
+#'# multiplication
+#'
 #'@export
 #'@importFrom methods new
 #'@importFrom methods checkAtAssignment
@@ -12,9 +38,9 @@ hashed.model.matrix <- function(object, data, hash_size = 2^24, transpose = TRUE
   stopifnot(hash_size >= 0)
   stopifnot(is.data.frame(data))
   tf <- terms.formula(object, data = data, specials = "tag")
-  retval <- new(.CSRMatrix)
+  retval <- new(.CSCMatrix)
   .hashed.model.matrix.dataframe(tf, data, hash_size, transpose, retval, keep.hashing_mapping)
-  class(retval) <- .CSRMatrix
+  class(retval) <- .CSCMatrix
   retval@Dimnames[[2]] <- paste(seq_len(retval@Dim[2]))
   retval
 }
--- R/matrix.R
+++ R/matrix.R
@@ -1,4 +1,23 @@
-setClass("CSRMatrix", representation(
+#'@title CSCMatrix
+#'@description The structure of \code{CSCMatrix} is the same
+#'as the structure of \code{dgCMatrix}. However, the
+#'\code{CSCMatrix} has weaker constraints compared to \code{dgCMatrix}.
+#'
+#'\code{CSCMatrix} supportes limited operators. The user should convert it to
+#'\code{dgCMatrix} if more comprehensive computation is required.
+#'@seealso \code{\link{dgCMatrix-class}}
+#'@details The \code{CSCMatrix} validates two constraints used in \code{dgCMatrix}:
+#'\itemize{
+#'  \item The row indices should be sorted with columns.
+#'  \item The row indices should be unique with columns.
+#'}
+#'Therefore, the result of matrix-vector should be the same.
+#'@examples
+#'# construct a CSCMatrix
+#'m <- hashed.model.matrix(~ ., CO2, 8)
+#'# convert it to dgCMatrix
+#'m2 <- as(m, "dgCMatrix")
+setClass("CSCMatrix", representation(
   i = "integer",
   p = "integer",
   Dim = "integer",
@@ -6,29 +25,29 @@ setClass("CSRMatrix", representation(
   x = "numeric",
   factors = "list"))
 
-.CSRMatrix <- "CSRMatrix"
-attr(.CSRMatrix, "package") <- .packageName
+.CSCMatrix <- "CSCMatrix"
+attr(.CSCMatrix, "package") <- .packageName
 
-setMethod("dim", signature(x = .CSRMatrix), function(x) x@Dim)
+setMethod("dim", signature(x = .CSCMatrix), function(x) x@Dim)
 
-setMethod("dim<-", signature(x = .CSRMatrix, value = "ANY"), function(x, value) {
+setMethod("dim<-", signature(x = .CSCMatrix, value = "ANY"), function(x, value) {
   x@Dim <- value
 })
 
-setMethod("%*%", signature(x = .CSRMatrix, y = "numeric"), function(x, y) {
+setMethod("%*%", signature(x = .CSCMatrix, y = "numeric"), function(x, y) {
   stopifnot(x@Dim[2] == length(y))
   Xv(x, y, numeric(x@Dim[1]))
 })
 
-setMethod("%*%", signature(x = "numeric", y = .CSRMatrix), function(x, y) {
+setMethod("%*%", signature(x = "numeric", y = .CSCMatrix), function(x, y) {
   stopifnot(y@Dim[1] == length(x))
   vX(x, y, numeric(y@Dim[2]))
 })
 
-setAs(.CSRMatrix, "dgCMatrix", function(from) todgCMatrix(from))
-setAs(.CSRMatrix, "matrix", function(from) tomatrix(from))
+setAs(.CSCMatrix, "dgCMatrix", function(from) todgCMatrix(from))
+setAs(.CSCMatrix, "matrix", function(from) tomatrix(from))
 
-setMethod("[", c(.CSRMatrix, "numeric", "numeric"), 
+setMethod("[", c(.CSCMatrix, "numeric", "numeric"), 
           function(x, i, j, ..., drop = TRUE) {
             iset <- head(seq(from = x@p[j], to = x@p[j + 1], by = 1), -1) + 1
             if (i %in% x@i[iset]) {
@@ -36,23 +55,23 @@ setMethod("[", c(.CSRMatrix, "numeric", "numeric"),
             } else 0
           })
 
-setMethod("[", c(.CSRMatrix, "numeric", "missing"),
+setMethod("[", c(.CSCMatrix, "numeric", "missing"),
           function(x, i, j, ..., drop = TRUE) {
             if (drop) return(.selectRow(x, i, drop)) else {
-              retval <- new(.CSRMatrix)
+              retval <- new(.CSCMatrix)
               .selectRow(x, i, drop, retval)
-              class(retval) <- .CSRMatrix
+              class(retval) <- .CSCMatrix
               retval@Dimnames[[2]] <- paste(seq_len(retval@Dim[2]))
               return(retval)
             }
           })
 
-setMethod("[", c(.CSRMatrix, "missing", "numeric"), 
+setMethod("[", c(.CSCMatrix, "missing", "numeric"), 
           function(x, i, j, ..., drop = TRUE) {
             if (drop) return(.selectColumn(x, j, drop)) else {
-              retval <- new(.CSRMatrix)
+              retval <- new(.CSCMatrix)
               .selectColumn(x, j, drop, retval)
-              class(retval) <- .CSRMatrix
+              class(retval) <- .CSCMatrix
               retval@Dimnames[[2]] <- paste(seq_len(retval@Dim[2]))
               return(retval)
             }

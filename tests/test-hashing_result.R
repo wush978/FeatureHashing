1e096f6d4d24c473a58c7feb0d4b90b042e93c6a
Wush Wu <wush@bridgewell.com>
2014-11-27 16:50:46 +0800

    increase coverage

--- tests/test-hashing_result.R
+++ tests/test-hashing_result.R
@@ -2,11 +2,14 @@ library(FeatureHashing)
 
 m <- hashed.model.matrix(~ ., CO2, hash_size = 2^10, keep.hashing_mapping = T)
 mapping <- as.list(attr(m, "mapping"))
+stopifnot(all(!duplicated(unlist(mapping) %% 2^10 + 1)))
 
-i <- hash_h(names(mapping))[2] %% 2^10 + 1
-j <- which(CO2$Plant == "Qn2")
-value <- hash_xi(names(mapping))[2]
-
-x <- m[i,]
-stopifnot(x[j] == value)
-stopifnot(x[-j] == 0)
+for(index in length(mapping)) {
+  i <- hash_h(names(mapping))[index] %% 2^10 + 1
+  j <- which(CO2$Plant == "Qn2")
+  value <- hash_xi(names(mapping))[index]
+  
+  x <- m[i,]
+  stopifnot(x[j] == value)
+  stopifnot(x[-j] == 0)
+}

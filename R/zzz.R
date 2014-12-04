5b0c8b033dbc6ea9bc3502587acc9977791bd494
Wush Wu <wush@bridgewell.com>
2014-12-04 17:20:07 +0800

    rewrite documentation

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,10 @@
 }
 
 #'@title test.tag
-#'@description This is a vector to demo the array-type
-#'feature.
+#'@description This is a vector to demo the concatenated data.
+#'@format For each element, the string represents the occurrence
+#' of different tags. For example, the string "1,27,19,25,tp,tw"
+#' of the first instance represents that the feature `1` is TRUE, the feature `27` is 
+#' TRUE, et. al. On the contrary, the missing feature such as `2` 
+#' is FALSE.
 test.tag <- NULL

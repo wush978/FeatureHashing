8a1e8c560a702538915f5890082551f5ab074c3b
Wush Wu <wush978@gmail.com>
2015-02-23 21:08:52 +0800

    correct test for new API

--- tests/test-as-dgCMatrix.R
+++ tests/test-as-dgCMatrix.R
@@ -44,7 +44,7 @@ if (require(RUnit)) {
     Treatment = contrasts(CO2$Treatment, contrasts = FALSE)
   )
   
-  m4 <- hashed.model.matrix(~ ., data = CO2, hash_size = 2^4)
+  m4 <- hashed.model.matrix(~ ., data = CO2, hash_size = 2^4, is.dgCMatrix = FALSE, transpose = TRUE)
   
   m5 <- as(m4, "dgCMatrix")
   invisible(capture.output(print(m5)))

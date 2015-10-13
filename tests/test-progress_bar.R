20cba1432a0ab755c057018133cc9c95c76e7681
Wush Wu <wush978@gmail.com>
2015-10-13 20:36:04 +0800

    relax test condition

--- tests/test-progress_bar.R
+++ tests/test-progress_bar.R
@@ -2,10 +2,10 @@ if (require(RUnit)) {
   library(methods)
   library(FeatureHashing)
   pd <- capture.output(m1 <- hashed.model.matrix(~ ., CO2, 2^5, transpose = TRUE, progress = TRUE))
-  checkTrue(all.equal(pd, c(
+  checkTrue(all.equal(paste(pd, collapse = ""), paste(c(
     "", 
     "0%   10   20   30   40   50   60   70   80   90   100%", 
     "|----|----|----|----|----|----|----|----|----|----|", 
     "***************************************************"
-  )))
+  ), collapse = "")))
 }
